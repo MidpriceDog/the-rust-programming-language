5464514d2d1facf795e4ee6c687f18056eff251c
MidpriceDog <ianpaul1313+1@gmail.com>
2024-03-11 05:17:38 -0400

    Slices examples

--- 4_understanding_ownership/the_slice_type/src/main.rs
+++ 4_understanding_ownership/the_slice_type/src/main.rs
@@ -1,14 +1,34 @@
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
+fn first_word(s: &String) -> &str {
+    let bytes: &[u8] = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i];
         }
     }
-    s.len()
+    &s[..]
+}
+
+fn other_slices() {
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[1, 3]);
 }
 
 fn main() {
-    let my_string = String::from("Hello there.");
-    println!("{}", first_word(&my_string));
+    let mut my_string: String = String::from("Hello there.");
+    let word = first_word(&my_string);
+    println!("{}", word);
+    my_string.clear();
+    // word_index has value 5 but no more string we can meaningfully use the
+    // value 5 with.
+    // We can use String slices provided by Rust to keep the variables synced.
+    // A string slice is a reference to part of a String.
+    let s = String::from("Hello world!");
+    // The type &str signifies a string slice.
+    let he = &s[..2];
+    let after_hello = &s[5..];
+    println!("{}", he);
+    println!("{}", after_hello);
+
+    other_slices();
 }
